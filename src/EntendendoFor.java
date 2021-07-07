1031d3f0052798289f2e6265b4a34dd6698f5793
Mariha Martins <mariha_martins@hotmail.com>
2021-07-07 18:58:40 -0300

    Atualização de arquivos

--- src/EntendendoFor.java
+++ src/EntendendoFor.java
@@ -2,7 +2,10 @@
 public class EntendendoFor {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		for(int i=1;i<=10;i++) {
+			System.out.println(i);
+		}
 
 	}
 
